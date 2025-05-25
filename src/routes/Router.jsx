7b27183d7e4e2e26fb4d173544b0baf68cabe7cd
aishah Ali <ayoshali13@gmail.com>
2025-05-25 06:03:13 +0300

    add all

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,7 +6,6 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Home from '../pages/Home';
 import About from '../pages/About';
-import Careers from '../pages/Careers';
 import Support from '../pages/Support';
 
 function Layout(){
